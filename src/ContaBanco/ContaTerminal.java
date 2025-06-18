c1a1c7cf5c889684c37bfa0dacd08d049bf11584
Luiza Almeida <luizaalmeida682@gmail.com>
2025-06-18 18:40:41 -0300

    meu projeto conta banco

--- src/ContaBanco/ContaTerminal.java
+++ src/ContaBanco/ContaTerminal.java
@@ -1,23 +1,34 @@
 package ContaBanco;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Cria um objeto Scanner para leitura de dados
+        DecimalFormat df = new DecimalFormat("0.00"); // Formata o saldo com duas casas decimais
 
-        System.out.println("Por favor, digite o número da Agência! ");
-        String numeroAgencia = scanner.next();
-        System.out.println("Por favor, digite o número da Conta! ");
+        // Solicita que o usuário digite o número da agência e armazena a resposta
+        System.out.println("Por favor, digite o número da Agência: ");
+        String numeroAgencia = scanner.nextLine();
+
+        // Solicita que o usuário digite o número da conta e armazena a resposta
+        System.out.println("Por favor, digite o número da Conta: ");
         int numeroConta = scanner.nextInt();
-        System.out.println("Por favor, digite seu nome! ");
-        String nomeCliente = scanner.next();
-        System.out.println("Por favor, digite seu saldo! ");
+        scanner.nextLine(); // limpa o buffer após ler o número
+
+        // Solicita que o usuário digite o nome do cliente e armazena a resposta
+        System.out.println("Por favor, digite seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicita que o usuário digite o saldo e armazena a resposta
+        System.out.println("Por favor, digite seu saldo: ");
         float saldo = scanner.nextFloat();
 
+        // Exibe uma mensagem final com os dados coletados do usuário
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco," +
-                " sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo +
-                " já está disponível para saque.");
+                " sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo R$ " +
+                df.format(saldo) + " já está disponível para saque.");
     }
 }
